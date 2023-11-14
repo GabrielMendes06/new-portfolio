f3fc12e0fb9b1b649ea710cee75b3d223518a4fc
Gabriel <mayara.alves@tracevia.com.br>
2023-11-14 16:49:59 -0300

    update changes

---
+++ src/app/components/global/Title.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react"
+
+interface TitleProps extends React.HTMLAttributes<HTMLParagraphElement>{
+    text: string | ReactNode
+    className?: string
+}
+
+const Title: React.FC<TitleProps> = ({text, className , ...rest}) => {
+    return <h1 className={`logo-text mx-3 px-2 title mb-3 ${className}`} {...rest}>{text}</h1>
+}
+
+export default Title
--- src/app/components/sections/home/SectionHome.tsx
+++ src/app/components/sections/home/SectionHome.tsx
@@ -4,6 +4,7 @@ import { motion } from "framer-motion";
 import Paragraph from "../../global/Paragraph";
 import { useEffect } from "react";
 import Typewriter from "react-ts-typewriter";
+import Title from "../../global/Title";
 
 const SectionHome: React.FC = () => {
 
@@ -18,9 +19,23 @@ const SectionHome: React.FC = () => {
         <>
             <OnlyHeader />
             <div className="container bg-transparent initial-content d-flex align-items-center section-home">
-                <Paragraph 
-                text={<Typewriter text='Olá, me chamo Gabriel Mendes, sou desenvolvedor especializado em Front-End, um prazer em conhece-lo.' />}
-                className="initial-text-apresentation"/>
+                <div className="d-flex flex-column initial-text-apresentation">
+                    <motion.div 
+                    initial={{ opacity: 0 }}
+                    animate={{
+                        y: [-100, 0],
+                        opacity: [0, 1]
+                    }}
+                    transition={{ duration: 1.5 }}>
+                        <Title text={<>
+                            <span className="span-title">Construindo</span> sistemas e designs <span className="span-title">Codificando</span> sempre da maneira mais <span className="span-title">limpa</span> possível :]
+                        </>}
+                            className="initial-title-apresentation" />
+                    </motion.div>
+                    <Paragraph
+                        text={<Typewriter text={'Olá, me chamo Gabriel Mendes,sou desenvolvedor especializado em Front-End, um prazer em conhece-lo.'} />}
+                        className="initial-paragraph-apresentation" />
+                </div>
                 <motion.div
                     initial={{ opacity: 0 }}
                     animate={{
