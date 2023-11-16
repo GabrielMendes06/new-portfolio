bc7ef957f8dc64353d9e50b60f266871dfcd46ff
Gabriel <mayara.alves@tracevia.com.br>
2023-11-16 11:15:59 -0300

    update mobile version

--- src/app/components/sections/home/SectionHome.tsx
+++ src/app/components/sections/home/SectionHome.tsx
@@ -6,72 +6,106 @@ import Typewriter from "react-ts-typewriter";
 import Title from "../../global/Title";
 import DevBackground from "../../../resources/images/sections/sectionHome/programacao.png";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const SectionHome: React.FC = () => {
-  return (
-    <>
-      <OnlyHeader />
-      <div className="container bg-transparent initial-content d-flex align-items-center section-home">
-        <div className="d-flex flex-column initial-text-apresentation">
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{
-              y: [-100, 0],
-              opacity: [0, 1],
-            }}
-            transition={{ duration: 1.5 }}
-          >
-            <Title
-              text={
-                <>
-                  A vida é uma constante oscilação entre a{" "}
-                  <span className="span-title">ânsia</span> de{" "}
-                  <span className="span-title">ter</span> e o{" "}
-                  <span className="span-title">tédio</span> de{" "}
-                  <span className="span-title">possuír</span>.
-                </>
-              }
-              className="initial-title-apresentation"
-            />
-          </motion.div>
-          <Paragraph
-            text={
-              <Typewriter
-                text={
-                  "Olá, me chamo Gabriel Mendes, sou desenvolvedor especializado em Front-End, um prazer em conhece-lo."
-                }
-              />
-            }
-            className="initial-paragraph-apresentation"
-          />
-        </div>
-        <motion.div
-          initial={{ x: 100, opacity: 0 }}
-          animate={{
-            x: [100, 0],
-            opacity: [0 , 1]
-          }}
-          transition={{ duration: 2 }}
-        >
-          <motion.div
-            initial={{  }}
-            animate={{
-              opacity: [1, 0, 0.8, 0, 1, 0.2]
-              
-            }}
-            transition={{
-              delay: 2,
-              repeat: Infinity,
-              duration: 2.5,
-              times: [0, 0.1, 0.25, 0.26, 1],
-            }}
-          >
-            <Image src={DevBackground} alt="Imagem de fundo" />
-          </motion.div>
-        </motion.div>
-      </div>
-    </>
-  );
+    
+    const [isSmall, setIsSmall] = useState(false);
+
+    useEffect(() => {
+        const checkScreenSize = () => {
+            const matchMedia = window.matchMedia("(max-width: 968px)");
+            setIsSmall(matchMedia.matches);
+        };
+
+        checkScreenSize();
+
+        window.addEventListener('resize', checkScreenSize);
+
+        return () => {
+            window.removeEventListener('resize', checkScreenSize);
+        };
+    }, []);
+
+    // Definição dos variants baseado no estado isSmall
+    const variants = isSmall ? {
+        animate: {
+            opacity: [0.3, 0, 0.2, 0, 0.3, 0.2]
+        },
+        transition: {
+            delay: 2,
+            repeat: Infinity,
+            duration: 2.5,
+            times: [0, 0.1, 0.25, 0.26, 1],
+        }
+    } : {
+        animate: {
+            opacity: [1, 0, 0.8, 0, 1, 0.2]
+        },
+        transition: {
+            delay: 2,
+            repeat: Infinity,
+            duration: 2.5,
+            times: [0, 0.1, 0.25, 0.26, 1],
+        }
+    };
+
+    return (
+
+        <>
+            <OnlyHeader />
+            <div className="container bg-transparent initial-content d-flex align-items-center section-home">
+                <div className="d-flex flex-column initial-text-apresentation">
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{
+                            y: [-100, 0],
+                            opacity: [0, 1],
+                        }}
+                        transition={{ duration: 1.5 }}
+                    >
+                        <Title
+                            text={
+                                <>
+                                    <span className="span-title">Construindo</span> sistemas e designs{" "}
+                                    <span className="span-title">codificando</span> sempre da maneira mais{" "}
+                                    <span className="span-title">limpa</span> possível :]
+                                </>
+                            }
+                            className="initial-title-apresentation"
+                        />
+                    </motion.div>
+                    <Paragraph
+                        text={
+                            <Typewriter
+                                text={
+                                    "Olá, me chamo Gabriel Mendes, sou desenvolvedor especializado em Front-End, prazer em conhece-lo."
+                                }
+                            />
+                        }
+                        className="initial-paragraph-apresentation"
+                    />
+                </div>
+                <motion.div
+                    initial={{ x: 100, opacity: 0 }}
+                    animate={{
+                        x: [100, 0],
+                        opacity: [0, 1]
+                    }}
+                    transition={{ duration: 2 }}
+                    className="animation-heartcode"
+                >
+                    <motion.div
+                        animate={variants.animate}
+                        transition={variants.transition}
+                    >
+                        <Image src={DevBackground} alt="Imagem de fundo" 
+                        priority/>
+                    </motion.div>
+                </motion.div>
+            </div>
+        </>
+    );
 };
 
 export default SectionHome;
