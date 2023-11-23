037c677affd97e0c8252a4580426aa7d5c7b95b9
Gabriel <mayara.alves@tracevia.com.br>
2023-11-23 11:01:56 -0300

    bug adjusted

--- src/app/components/sections/SectionAbout/SectionAbout.tsx
+++ src/app/components/sections/SectionAbout/SectionAbout.tsx
@@ -4,21 +4,9 @@ import Image from 'next/image'
 import Paragraph from '../../global/Paragraph'
 import Title from '../../global/Title'
 import SmallIcon from '../../global/SmallIcon'
-import { motion, useAnimation } from 'framer-motion'
-import { useInView } from 'react-intersection-observer'
-import { useEffect } from 'react'
+import { motion } from 'framer-motion'
 
 const SectionAbout: React.FC = () => {
-
-    const controls = useAnimation();
-    const [ref, inView] = useInView();
-
-    useEffect(() => {
-        console.log(inView)
-        if (inView) {
-            controls.start('show')
-        }
-    }, [controls, inView])
 
     const FadeIn = {
         hidden: { opacity: 0 },
@@ -49,22 +37,25 @@ const SectionAbout: React.FC = () => {
         <div className="container bg-transparent section-content d-flex align-items-center justify-content-center section-about">
             <div className='bg-card card-content row d-flex p-4'>
                 <motion.div className='col-sm-6 middle-card-div'
-                    ref={ref}
-                    animate={controls}
                     variants={FadeIn}
                     initial='hidden'
+                    whileInView={'show'}
+                    viewport={{once: true}}
                 >
-                    <Image
-                        src={Cartoon}
-                        alt='Dev Cartoon'
-                    />
+                    <motion.div variants={item}>
+                        <Image
+                            src={Cartoon}
+                            alt='Dev Cartoon'
+                        />
+                    </motion.div>
+
                 </motion.div>
                 <div className='col-sm-6 d-flex flex-column align-items-start middle-card-div'>
                     <motion.div
-                        ref={ref}
-                        animate={controls}
                         variants={FadeIn}
                         initial='hidden'
+                        whileInView={'show'}
+                        viewport={{once: true}}
                     >
                         <motion.div variants={item}>
                             <Title
@@ -72,7 +63,8 @@ const SectionAbout: React.FC = () => {
                                 className='about-subtitle span-title mt-5'
                             />
                         </motion.div>
-                        <motion.div variants={item}>
+                        <motion.div
+                            variants={item}>
                             <Title
                                 text='Gabriel Mendes'
                                 className='about-title'
@@ -92,10 +84,10 @@ const SectionAbout: React.FC = () => {
                         </motion.div>
                     </motion.div>
                     <motion.div className='d-flex icon-container'
-                        ref={ref}
-                        animate={controls}
                         variants={FadeIn}
-                        initial='hidden'>
+                        initial='hidden'
+                        whileInView={'show'}
+                        viewport={{once: true}}>
                         <SmallIcon
                             href='#'
                             className='bi bi-linkedin'
