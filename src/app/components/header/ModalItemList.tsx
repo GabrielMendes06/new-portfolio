841c7f54eacd29b5f4260a67471889087dbe4b31
Gabriel <mayara.alves@tracevia.com.br>
2023-12-14 15:17:55 -0300

    add logic burguer

--- src/app/components/header/ModalItemList.tsx
+++ src/app/components/header/ModalItemList.tsx
@@ -1,8 +1,22 @@
+import { useEffect } from "react";
+
 interface ModalItemlistProps extends React.LiHTMLAttributes<HTMLLIElement> {
   text: string;
+  onClose: Function;
+  rollSectionFunction: (e: any) => void
 }
 
-const ModalItemlist: React.FC<ModalItemlistProps> = ({ text, ...rest }) => {
+const ModalItemlist: React.FC<ModalItemlistProps> = ({ 
+  text, 
+  onClose, 
+  rollSectionFunction, 
+  ...rest  
+}) => {
+
+  function handleItemClick() {
+    onClose(); // Chame a função onClose para fechar o modal
+  }
+
   return (
     <li
       className="
@@ -13,6 +27,10 @@ const ModalItemlist: React.FC<ModalItemlistProps> = ({ text, ...rest }) => {
       text-center 
       m-3 
       cursor-pointer"
+      onClick={(e) => {
+        handleItemClick();
+        rollSectionFunction(e);
+      }} 
       {...rest}
     >
       {text}
--- src/app/components/header/NavbarModal.tsx
+++ src/app/components/header/NavbarModal.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-import ModalItemlist from "./ModalItemList";
+import ModalItemList from "./ModalItemList";
 
 function Example() {
   const [fullscreen, setFullscreen] = useState<string | true | undefined>(true);
@@ -9,6 +9,23 @@ function Example() {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
   const [scrolled, setScrolled] = useState(false);
   const verification = scrolled ? 'text-light' : 'default-text'
+
+  const handleNavClick = (e: any, id: any) => {
+    e.preventDefault();
+    const section = document.querySelector(`#${id}`);
+
+    if (section instanceof HTMLElement) {
+      const offsetTop = section.offsetTop - 100; // 50 pixels acima da seção
+      window.scrollTo({
+        top: offsetTop,
+        behavior: 'smooth'
+      });
+    }
+  };
+
+  function handleCloseModal() {
+    setShow(false);
+  }
 
   function checkThemeClass() {
     setIsDarkTheme(document.body.classList.contains('dark-theme'));
@@ -50,6 +67,7 @@ function Example() {
         fullscreen={fullscreen}
         onHide={() => setShow(false)}
         className="modal-bg-color"
+        id="myModal"
       >
         <Modal.Header
           className="
@@ -57,18 +75,32 @@ function Example() {
           border-0"
           closeButton={true}
           closeVariant={isDarkTheme ? 'white' : ''}
-
         ></Modal.Header>
         <Modal.Body className="
         modal-bg-color 
         d-flex flex-column 
         list-none 
         align-items-center">
-          <ModalItemlist text="Início" />
-          <ModalItemlist text="Sobre mim" />
-          <ModalItemlist text="Conhecimentos" />
-          <ModalItemlist text="Projetos" />
-          <ModalItemlist text="Contato" />
+          <ModalItemList
+            text="Início"
+            onClose={handleCloseModal}
+            rollSectionFunction={(e) => handleNavClick(e, 'home')} />
+          <ModalItemList
+            text="Sobre mim"
+            onClose={handleCloseModal}
+            rollSectionFunction={(e) => handleNavClick(e, 'about')} />
+          <ModalItemList
+            text="Conhecimentos"
+            onClose={handleCloseModal}
+            rollSectionFunction={(e) => handleNavClick(e, 'skills')} />
+          <ModalItemList
+            text="Projetos"
+            onClose={handleCloseModal}
+            rollSectionFunction={(e) => handleNavClick(e, 'projects')} />
+          <ModalItemList
+            text="Contato"
+            onClose={handleCloseModal}
+            rollSectionFunction={(e) => handleNavClick(e, 'contact')} />
         </Modal.Body>
       </Modal>
     </>
