830b3d4d4d9245cfba7c67cdf2134a4d37cdf878
Gabriel <mayara.alves@tracevia.com.br>
2024-01-08 13:26:58 -0300

    update

--- src/app/components/sections/SectionHome/SectionHome.tsx
+++ src/app/components/sections/SectionHome/SectionHome.tsx
@@ -112,7 +112,7 @@ const SectionHome: React.FC = () => {
                                 duration: 0.1
                             }
                         }}>
-                        <DefaultButton text="Download CV" link="https://drive.google.com/file/d/1WGetBZnnsUGENoobDE6CMUw9W2Qrfyw3/view?usp=sharing" className="cv-button" />
+                        <DefaultButton text="Download CV" link="https://drive.google.com/file/d/1LTXDj8UbxdmwvbnpMpeXjb50dtHy_Ei_/view?usp=sharing" className="cv-button" />
                     </motion.div>
                 </div>
                 <motion.div
